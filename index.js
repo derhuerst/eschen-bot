9c7f0e7fa6d4ffa5daa1fa6d9e82088ccbc5e8bc
Jannis R <mail@jannisr.de>
2018-04-14 02:21:50 +0200

    Telegraf

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 'use strict'
 
 const {createHash} = require('crypto')
-const Bot = require('node-telegram-bot-api')
+const Bot = require('telegraf')
+const url = require('url')
+const escape = require('js-string-escape')
+const through = require('through2')
 
 const beers = require('./lib/beers.json')
 const watching = require('./lib/watching')
 const onNewBeer = require('./lib/on-new-beer')
+
+const TOKEN = process.env.TOKEN
+if (!TOKEN) {
+	console.error('Missing TOKEN env var.')
+	process.exit(1)
+}
 
 const sha1 = (str) => {
 	const hash = createHash('sha1')
@@ -13,14 +22,9 @@ const sha1 = (str) => {
 	return hash.digest('hex')
 }
 
-const log = (msg) => {
-	console.info(sha1(msg.chat.id + '').slice(0, 16), msg.text)
-}
-
-const TOKEN = process.env.TOKEN
-if (!TOKEN) {
-	console.error('Missing TOKEN env var.')
-	process.exit(1)
+const logErr = (err) => {
+	if (process.env.NODE_ENV === 'dev') console.error(err)
+	else console.error(err && err.message || (err + ''))
 }
 
 const startedMsg = `\
@@ -38,40 +42,57 @@ Shit! Irgendwas stimmt hier nicht. Bitte probier das noch mal.`
 const beerMsg = (beer) => `\
 Ab heute kannst du im Eschenbräu ein *${beer.name}* genießen! 🍻
 
-> ${description}
+> ${beer.description}
 
-${gravity} Stammwürze
-${alcohol}% Alkohol`
+${beer.gravity} Stammwürze
+${beer.alcohol}% Alkohol`
 
 const listMsg = beers.map(b => `\`${b.date}\` ${b.name}\n`).join('')
 
-const bot = new Bot(TOKEN, {polling: true})
-const withMarkdown = {parse_mode: 'Markdown'}
+const bot = new Bot(TOKEN)
 
-bot.on('message', (msg) => {
-	if (!msg.text) return
-	log(msg)
-	const user = msg.chat.id
+bot.use(async (ctx, next) => {
+	if (!ctx.chat || !ctx.chat.id) return null
+	ctx.user = ctx.chat.id
 
-	if (msg.text.slice(0, 5).toLowerCase() === '/list') {
-		bot.sendMessage(user, listMsg, withMarkdown)
-	} else if (msg.text.slice(0, 6).toLowerCase() === '/start') {
-		watching.add(user, (err) => {
-			if (err) bot.sendMessage(user, errMsg, withMarkdown)
-			else bot.sendMessage(user, startedMsg, withMarkdown)
-		})
-	} else if (msg.text.slice(0, 5).toLowerCase() === '/stop') {
-		watching.del(user, (err) => {
-			if (err) bot.sendMessage(user, errMsg, withMarkdown)
-			else bot.sendMessage(user, stoppedMsg, withMarkdown)
-		})
-	} else {
-		bot.sendMessage(user, helpMsg, withMarkdown)
-	}
+	const t0 = Date.now()
+	await next(ctx)
+	const d = Date.now() - t0
+
+	const msg = ctx.message
+	console.info([
+		d + 'ms',
+		msg && msg.date || '[unknown date]',
+		ctx.user,
+		(
+			msg && msg.text && escape(msg.text.slice(0, 30)) || '[no message]'
+		)
+	].join(' '))
 })
 
-bot.on('polling_error', (err) => {
-	console.error(err)
+bot.command('/list', (ctx) => {
+	return ctx.replyWithMarkdown(listMsg)
+})
+bot.command('/start', async (ctx) => {
+	try {
+		await watching.add(ctx.user)
+		await ctx.replyWithMarkdown(startedMsg)
+	} catch (err) {
+		logErr(err)
+		await ctx.replyWithMarkdown(errMsg)
+	}
+})
+bot.command('/stop', async (ctx) => {
+	try {
+		await watching.del(ctx.user)
+		await ctx.replyWithMarkdown(stoppedMsg)
+	} catch (err) {
+		logErr(err)
+		await ctx.replyWithMarkdown(errMsg)
+	}
+})
+bot.use((ctx) => {
+	return ctx.replyWithMarkdown(helpMsg)
 })
 
 onNewBeer((beer) => {
@@ -79,12 +100,50 @@ onNewBeer((beer) => {
 	let receivers = 0
 
 	watching.all()
-	.on('data', (user) => {
-		bot.sendMessage(user, msg, withMarkdown)
+	.pipe(through.obj((user, _, cb) => {
 		receivers++
-	})
-	.once('end', () => {
+		bot.telegram.sendMessage(user, msg, {parse_mode: 'Markdown'})
+		.then(() => cb())
+		.catch(cb)
+	}))
+	.once('finish', () => {
 		console.info(`Notified ${receivers} people about ${beer.name}.`)
 	})
-	.once('error', console.error)
+	.once('error', logErr)
 })
+
+if (process.env.NODE_ENV === 'dev') {
+	console.info('using polling')
+
+	bot.telegram.deleteWebhook()
+	.then(() => bot.startPolling())
+	.catch(logErr)
+} else {
+	console.info('using web hook')
+
+	const WEB_HOOK_HOST = process.env.WEB_HOOK_HOST
+	if (!WEB_HOOK_HOST) {
+		console.error('Missing WEB_HOOK_HOST env var.')
+		process.exit(1)
+	}
+	const WEB_HOOK_PATH = process.env.WEB_HOOK_PATH
+	if (!WEB_HOOK_PATH) {
+		console.error('Missing WEB_HOOK_PATH env var.')
+		process.exit(1)
+	}
+	const WEB_HOOK_PORT = process.env.WEB_HOOK_PORT && parseInt(process.env.WEB_HOOK_PORT)
+	if (!WEB_HOOK_PORT) {
+		console.error('Missing WEB_HOOK_PORT env var.')
+		process.exit(1)
+	}
+
+	bot.webhookReply = false
+	bot.telegram.setWebhook(url.format({
+		protocol: 'https',
+		host: WEB_HOOK_HOST,
+		pathname: WEB_HOOK_PATH
+	}))
+	bot.startWebhook(WEB_HOOK_PATH, null, WEB_HOOK_PORT)
+}
+
+bot.catch(console.error)
